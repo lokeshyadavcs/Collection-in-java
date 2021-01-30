c9bc3e9bc9c6b7e46b5a7bc5ca779a3f9382767d
lokeshyadavcs <thatslokesh1@gmail.com>
2021-01-30 19:53:04 +0530

    Add fun ini arraylist

---
+++ AddarrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AddarrayList
+ */
+public class AddarrayList {
+
+    public static void main(String[] args) {
+        List <String> list = new ArrayList<>();
+        list.add("ram");
+        list.add("baam");
+        list.add("bhuvan baam");
+        list.add("aram");
+        System.out.println(list);
+
+
+    }
+}
