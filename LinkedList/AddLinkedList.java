50c29210ca144cb909ab8aacb71bf1b959000368
lokeshyadavcs <thatslokesh1@gmail.com>
2021-01-30 22:05:17 +0530

    Add fun in linkedList

---
+++ LinkedList/AddLinkedList.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class AddLinkedList {
+
+    public static void main(String[] args) {
+        List <String> list2 = new LinkedList<>();
+        
+        list2.add("I");
+        list2.add("Know");
+        list2.add("Java");
+        list2.add("Programing");
+        list2.add("hmm");
+        System.out.println(list2);
+
+    }
+}
