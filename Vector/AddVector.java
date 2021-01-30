2f7a5eae330413bfd5302b1f68fbb59dc37dfada
lokeshyadavcs <thatslokesh1@gmail.com>
2021-01-30 22:08:39 +0530

    Add fun in Vector

---
+++ Vector/AddVector.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+
+public class AddVector {
+    public static void main(String[] args) {
+        Vector <String> list3= new Vector<>();
+        list3.add("This");
+        list3.add("is");
+        list3.add("it");
+        System.out.println(list3);
+    }
+    
+}
