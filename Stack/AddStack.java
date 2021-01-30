8982f7a016f2be3d6011ca81276aad740fe6dc63
lokeshyadavcs <thatslokesh1@gmail.com>
2021-01-30 22:12:02 +0530

    Add fun in Stack

---
+++ Stack/AddStack.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class AddStack {
+    public static void main(String[] args) {
+        Stack <String> list4= new Stack<>();
+        
+        list4.add("satck");
+        list4.add("is ");
+        list4.add("subclass");
+        list4.add("of ");
+        list4.add("vector class");
+        System.out.println(list4);
+    }
+}
