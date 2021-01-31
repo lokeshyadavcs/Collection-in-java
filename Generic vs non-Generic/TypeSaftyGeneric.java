ab21b03d1540de18b6896b2107fedefce7e16e82
lokeshyadavcs <thatslokesh1@gmail.com>
2021-01-31 17:39:56 +0530

    Type safty using Generic

---
+++ Generic vs non-Generic/TypeSaftyGeneric.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class TypeSaftyGeneric {
+    public static void main(String[] args) {
+         // Creating an ArrayList with String specified 
+         ArrayList<String> al = new ArrayList<String>(); 
+  
+         al.add("Sachin"); 
+         al.add("Rahul"); 
+   
+         // Now Compiler doesn't allow this 
+         al.add(10); 
+   
+         String s1 = al.get(0); 
+         String s2 = al.get(1); 
+         String s3 = al.get(2); 
+    }
+}
