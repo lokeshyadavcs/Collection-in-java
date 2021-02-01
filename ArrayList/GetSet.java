059772372c14d3b066a66941e0e32edf62db2825
lokeshyadavcs <thatslokesh1@gmail.com>
2021-02-01 21:07:54 +0530

    get set method in ArrayList

--- ArrayList/GetSet.java
+++ ArrayList/GetSet.java
@@ -1,5 +1,20 @@
+//The get() method returns the element at the specified index, whereas the set() method changes the element.
+import java.util.ArrayList;
+
 public class GetSet {
     public static void main(String[] args) {
-        
+        ArrayList<String> al=new ArrayList<String>();  
+  al.add("Mango");  
+  al.add("Apple");  
+  al.add("Banana");  
+  al.add("Grapes");  
+  //accessing the element    
+  System.out.println("Returning element: "+al.get(1));//it will return the 2nd element, because index starts from 0  
+  //changing the element  
+  al.set(1,"Dates");  
+  //Traversing list  
+  for(String fruit:al)    
+    System.out.println(fruit);    
+  
     }
 }
